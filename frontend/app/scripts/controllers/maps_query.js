c1fc03300674f4a44f73770f2a3c2f5dfdae4b83
David De Sousa <davidesousa@gmail.com>
2014-12-21 17:40:00 -0430

    Click on a map and it shows the service areas that serve that point

--- frontend/app/scripts/controllers/maps_query.js
+++ frontend/app/scripts/controllers/maps_query.js
@@ -12,7 +12,42 @@ var mozioApp = angular.module('mozioApp');
 mozioApp.controller('MapsQueryController', [
   '$scope',
   'uiGmapGoogleMapApi',
-  function ($scope, uiGmapGoogleMapApi) {
+  'Area',
+  function ($scope, uiGmapGoogleMapApi, Area) {
     uiGmapGoogleMapApi.then(function(maps){
+      $scope.map = {
+        center: {
+          latitude: 53.406754,
+          longitude: -2.158843
+        },
+        pan: true,
+        zoom: 14,
+        options: {
+        },
+        events: {},
+        bounds: {},
+        polys: [],
+        draw: undefined
+      };
+
+
+      $scope.serviceEventHandler = {
+        'click': function(googlePoly, eventName, ngModel, args){
+          if (args[0]._alreadyCalled){
+            return
+          }
+          args[0]._alreadyCalled = true;
+          google.maps.event.trigger(googlePoly.map, 'click', args[0]);
+          $scope.handlingClick = false;
+        }
+      };
+
+      $scope.mapsEventHandler = {
+        'click': function(maps, eventName, args){
+          var latLng = args[0].latLng;
+
+          $scope.areas = Area.$search({'lat': latLng.lat(), 'long': latLng.lng()});
+        }
+      };
     });
 }]);
--- maps/views.py
+++ maps/views.py
@@ -1,8 +1,24 @@
 from .models import Area
 from .serializers import AreaSerializer
 from rest_framework import viewsets
+from django.contrib.gis.geos import Point
+from rest_framework.response import Response
 
 
 class AreaViewSet(viewsets.ModelViewSet):
     queryset = Area.objects.all()
     serializer_class = AreaSerializer
+
+    def list(self, request):
+        latitude = request.GET.get('lat', None)
+        longitude = request.GET.get('long', None)
+
+        if latitude and longitude:
+            p = Point([float(longitude), float(latitude)])
+            queryset = self.queryset.filter(poly__contains=p)
+        else:
+            queryset = self.queryset.none()
+
+        serializer = AreaSerializer(queryset, many=True)
+
+        return Response(serializer.data)
